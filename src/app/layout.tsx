b0498bd73e0ee96fcaaf0652c460df7aa038e27f
sumin <suminkim.me@gmail.com>
2023-09-25 21:03:16 +0900

    Fix: 레이아웃 작업, 홈 페이지 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,81 @@
+import Header from '@/src/common/containers/layouts/Header';
 import './globals.css';
 
-export const metadata = {
-  title: 'sumDev',
-  description: 'Hello, world!',
-};
+import { cookies } from 'next/headers';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { Database } from '@/database.types';
+import Footer from '@/src/common/containers/layouts/Footer';
 
-export default function RootLayout({
+export const dynamic = 'force-dynamic';
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
   return (
     <html lang="en">
       <body>
-        {/* Header */}
-        <main className="min-h-screen flex justify-center">
-          <div className="max-w-screen-lg flex flex-col items-center w-full">
-            {children}
-          </div>
-        </main>
+        <Header user={user} />
+        <div className="min-h-screen flex flex-col justify-between gap-10">
+          <main className="flex justify-center">
+            <div className="max-w-screen-md flex flex-col items-center px-4 py-4 w-full mt-20">
+              {children}
+            </div>
+          </main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
 }
+
+const title = 'sumDev';
+const description = 'Front-end Developer, Becoming a solo engineer';
+const coverUrl =
+  'https://res.cloudinary.com/duinj0dld/image/upload/v1695111909/sumDev-cover_c2ptxd.webp';
+
+export const metadata = {
+  title,
+  description,
+  keywords: 'sumDev, sum-in.me',
+  metadataBase: new URL('https://linkloud.xyz'), // TODO: 수정
+  alternates: {
+    canonical: '/',
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    viewportFit: 'contain',
+  },
+  openGraph: {
+    title,
+    description,
+    url: 'https://linkloud.xyz', // TODO: 수정
+    type: 'website',
+    images: [
+      {
+        url: coverUrl,
+        alt: 'sumDev site cover Image',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    images: [
+      {
+        url: coverUrl,
+        alt: 'sumDev site cover Image',
+      },
+    ],
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,80 @@
-import Link from 'next/link';
-import { cookies } from 'next/headers';
+import { Database } from '@/database.types';
+import PostItem from '@/src/features/postList/components/PostItem';
+import ReadMoreButton from '@/src/features/postList/components/ReadMoreButton';
+import ProductList from '@/src/features/products/containers/ProductList';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import LogoutButton from '@/src/features/login/components/LogoutButton';
+import { cookies } from 'next/headers';
 
 export const dynamic = 'force-dynamic';
 
 export default async function HomePage() {
-  const supabase = createServerComponentClient({ cookies });
+  const supabase = createServerComponentClient<Database>({ cookies });
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  let { data: blogPosts, error: blogPostsError } = await supabase
+    .from('post')
+    .select('*')
+    .eq('type', 'blog')
+    .eq('is_public', true)
+    .range(0, 2);
+
+  let { data: notes, error: notesError } = await supabase
+    .from('post')
+    .select('*')
+    .eq('type', 'note')
+    .eq('is_public', true)
+    .range(0, 2);
+
+  if (blogPostsError || notesError) {
+    // TODO: Error UI
+    // TODO: 404 Page
+    return <div>Error</div>;
+  }
 
   return (
-    <div className="w-full flex flex-col items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
-          <div />
-          <div>
-            {user ? (
-              <div className="flex items-center gap-4">
-                Hey, {user.email}!
-                <LogoutButton />
-              </div>
-            ) : (
-              <Link
-                href="/login"
-                className="py-2 px-4 rounded-md no-underline border border-zinc-400"
-              >
-                Login
-              </Link>
-            )}
+    <div className="w-full flex flex-col items-center gap-16">
+      <section className="w-full">
+        <p className="text-3xl font-bold mb-10">Writing</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div className="flex-grow">
+            <p className="text-xl font-semibold text-zinc-700">Recent Posts</p>
+            <div className="flex justify-end mb-7">
+              <ReadMoreButton path="/blog" />
+            </div>
+            <ul className="flex flex-col gap-9">
+              {blogPosts?.map((post) => (
+                <PostItem
+                  id={post.id}
+                  title={post.title}
+                  description={post.description}
+                  created_at={post.created_at}
+                  views={post.views}
+                />
+              ))}
+            </ul>
+          </div>
+          <div className="flex-grow">
+            <p className="text-xl font-semibold text-zinc-700">Recent Notes</p>
+            <div className="flex justify-end mb-7">
+              <ReadMoreButton path="/note" />
+            </div>
+            <ul className="flex flex-col gap-9">
+              {notes?.map((post) => (
+                <PostItem
+                  id={post.id}
+                  title={post.title}
+                  description={post.description}
+                  created_at={post.created_at}
+                  views={post.views}
+                />
+              ))}
+            </ul>
           </div>
         </div>
-      </nav>
+      </section>
+      <section className="w-full">
+        <p className="text-3xl font-bold mb-10">Products</p>
+        <ProductList />
+      </section>
     </div>
   );
 }
