8fc33e67332b8c022b0009e78dd4488c647ab457
sumin <suminkim.me@gmail.com>
2023-10-01 19:45:59 +0900

    Fix: 404page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,7 @@
-import Link from 'next/link';
-
 const NotFoundPage = () => {
   return (
-    <div className="flex flex-col items-center gap-5">
+    <div className="flex justify-center h-full items-center gap-5 pt-10">
       <h4 className="text-2xl font-semibold">{`⚠️ Page Not Found ⚠️`}</h4>
-      <Link
-        href="/"
-        className="px-4 py-2 bg-zinc-200 rounded-md dark:bg-opacity-20"
-      >
-        Go Home
-      </Link>
     </div>
   );
 };
