974931b620b863d5e4f7364188b7b85f753913e2
sumin <suminkim.me@gmail.com>
2023-11-04 21:41:41 +0900

    Add: private post page

---
+++ src/app/private-post/page.tsx
@@ -0,0 +1,45 @@
+import { cookies } from 'next/headers';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { Database } from '@/database.types';
+import PostItem from '@/src/features/posts/components/PostItem';
+
+export const dynamic = 'force-dynamic';
+
+export default async function PrivatePostsPage() {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const { data: user, error } = await supabase.auth.getUser();
+
+  if (error?.status === 401) {
+    // 관리자 외 접근 금지 페이지이므로 페이지를 인지하지 못하도록 404 표시
+    return (
+      <div className="flex justify-center h-full items-center gap-5 pt-10">
+        <h4 className="text-2xl font-semibold">{`⚠️ Page Not Found ⚠️`}</h4>
+      </div>
+    );
+  }
+
+  let { data } = await supabase
+    .from('post')
+    .select('*')
+    .eq('is_public', false)
+    .order('created_at', { ascending: false });
+
+  if (!data || data.length === 0) {
+    return <div>No Post</div>;
+  }
+
+  return (
+    <ul className="flex flex-col gap-10">
+      {data.map((item) => (
+        <PostItem
+          id={item.id}
+          key={item.id}
+          title={item.title}
+          description={item.description}
+          created_at={item.created_at}
+        />
+      ))}
+    </ul>
+  );
+}
